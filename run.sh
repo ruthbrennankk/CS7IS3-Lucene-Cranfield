9d6057b4c0016e10fc79251cc04ac5d2682636ab
Ruth Brennan <47035857+ruthbrennankk@users.noreply.github.com>
2021-10-13 15:29:02 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,25 +1,25 @@
 mvn clean package -DskipTests
 java -jar target/brennar5-lucene-cranfield-1.0.jar
 
-trec_eval -m map -m gm_map -m P.5 ~/lucene-cranfield/src/cran/formatted_qrel.test ~/lucene-cranfield/src/cran/custom_classic.results > ~/lucene-cranfield/performance/custom_classic.txt
-trec_eval -m map -m gm_map -m P.5 ~/lucene-cranfield/src/cran/formatted_qrel.test ~/lucene-cranfield/src/cran/custom_bm25.results > ~/lucene-cranfield/performance/custom_bm25.txt
-trec_eval -m map -m gm_map -m P.5 ~/lucene-cranfield/src/cran/formatted_qrel.test ~/lucene-cranfield/src/cran/english_classic.results > ~/lucene-cranfield/performance/english_classic.txt
-trec_eval -m map -m gm_map -m P.5 ~/lucene-cranfield/src/cran/formatted_qrel.test ~/lucene-cranfield/src/cran/english_bm25.results > ~/lucene-cranfield/performance/english_bm25.txt
+trec_eval -m map -m gm_map -m P.5 ~/CS7IS3-Lucene-Cranfield/src/cran/formatted_qrel.test ~/CS7IS3-Lucene-Cranfield/src/cran/custom_classic.results > ~/CS7IS3-Lucene-Cranfield/performance/custom_classic.txt
+trec_eval -m map -m gm_map -m P.5 ~/CS7IS3-Lucene-Cranfield/src/cran/formatted_qrel.test ~/CS7IS3-Lucene-Cranfield/src/cran/custom_bm25.results > ~/CS7IS3-Lucene-Cranfield/performance/custom_bm25.txt
+trec_eval -m map -m gm_map -m P.5 ~/CS7IS3-Lucene-Cranfield/src/cran/formatted_qrel.test ~/CS7IS3-Lucene-Cranfield/src/cran/english_classic.results > ~/CS7IS3-Lucene-Cranfield/performance/english_classic.txt
+trec_eval -m map -m gm_map -m P.5 ~/CS7IS3-Lucene-Cranfield/src/cran/formatted_qrel.test ~/CS7IS3-Lucene-Cranfield/src/cran/english_bm25.results > ~/CS7IS3-Lucene-Cranfield/performance/english_bm25.txt
 
 echo " "
 echo "Performance of Search Engines"
 echo " "
 
 echo "custom_classic"
-cat ~/lucene-cranfield/performance/custom_classic.txt
+cat ~/CS7IS3-Lucene-Cranfield/performance/custom_classic.txt
 echo " "
 echo "custom_bm25"
-cat ~/lucene-cranfield/performance/custom_bm25.txt
+cat ~/CS7IS3-Lucene-Cranfield/performance/custom_bm25.txt
 echo " "
 echo "english_classic"
-cat ~/lucene-cranfield/performance/english_classic.txt
+cat ~/CS7IS3-Lucene-Cranfield/performance/english_classic.txt
 echo " "
 echo "english_bm25"
-cat ~/lucene-cranfield/performance/english_bm25.txt
+cat ~/CS7IS3-Lucene-Cranfield/performance/english_bm25.txt
 
 echo " "
